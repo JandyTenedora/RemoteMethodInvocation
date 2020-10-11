5761d568e3571ff5ca9b25516be20af0ece93a91
JandyTenedora <jandy.tenedora@hotmail.com>
2020-10-11 18:58:27 +1100

    Adding jfx features

--- RemoteMethodInvocation/src/main/scala/org/example/RMIChatClient.scala
+++ RemoteMethodInvocation/src/main/scala/org/example/RMIChatClient.scala
@@ -1,10 +1,53 @@
 package org.example
 
-// 1. Make remote interfaces (traits)
+import java.rmi.Naming
+import java.rmi.server.UnicastRemoteObject
+
+import scalafx.application.JFXApp
+import scalafx.scene.control.TextInputDialog
+
+// 1. Make remote interfaces (traits) annoted by @remote
+// 2. Make implementation that extends unicastremoteobject and traits
+// 3. Need server to bind itself to RMI registry
+// 4. Client does a name lookup
+// 5. Need an RMI registry running somewhere
+
+@remote trait RemoteClient{
+  def name: String
+  def message(sender: RemoteClient, text:String): Unit
+  def clientUpdate(clients: Seq[RemoteClient]): Unit
+
+
+}
 
 
 
-object RMIChatClient {
-  val testvalue = ":hello"
+object RMIChatClient extends UnicastRemoteObject with JFXApp with RemoteClient {
 
+  val dialog = new TextInputDialog("localhost")
+  dialog.title = "Server Machine"
+  dialog.contentText = "Choose a server to connect to"
+  dialog.headerText = "Server Name"
+  val (_name,server) = dialog.showAndWait() match {
+    case Some(machine) =>
+      Naming.lookup(s"rmi://$machine/ChatServer") match {
+        case server: RemoteServer =>
+          val dialog = new TextInputDialog("")
+          dialog.title = "Chat Name"
+          dialog.contentText = "What name do you want to go by?"
+          dialog.headerText = "User Name"
+          dialog.showAndWait() match{
+            case Some(name) => (name,server)
+            case None => sys.exit(0)
+          }
+        case _ =>
+          println("Error")
+          sys.exit(0)
+      }
+    case None => sys.exit(0)
+  }
+
+  def name: String = _name
+  def message(sender: RemoteClient, text:String): Unit = ???
+  def clientUpdate(clients: Seq[RemoteClient]): Unit = ???
 }
--- RemoteMethodInvocation/src/main/scala/org/example/RMIChatServer.scala
+++ RemoteMethodInvocation/src/main/scala/org/example/RMIChatServer.scala
@@ -1,5 +1,65 @@
 package org.example
 
-object RMIChatServer {
+import java.rmi.server.UnicastRemoteObject
+import java.rmi.{Naming, RemoteException}
+import java.rmi.registry.LocateRegistry
 
+import scalafx.scene.control.TextInputDialog
+
+import scala.collection.mutable
+
+@remote trait RemoteServer{
+  def connect(client: RemoteClient): Unit
+  def disconnect(client: RemoteClient): Unit
+  def getClient: Seq[RemoteClient]
+  def publicMessage(client: RemoteClient, text:String): Unit
+
+}
+
+object RMIChatServer extends UnicastRemoteObject with App with RemoteServer {
+
+  LocateRegistry.createRegistry(1099)
+
+  private val clients = mutable.Buffer[RemoteClient]()
+
+  def connect(client: RemoteClient): Unit = {
+    clients += client
+    sendUpdate
+
+  }
+  def disconnect(client: RemoteClient): Unit = {
+    clients -= client
+    sendUpdate
+
+  }
+  def getClient: Seq[RemoteClient] = {
+    clients
+    /* Some Notes
+    Object outputstreams can only pass through things which are serializable
+    So with RMI's there are 2 ways to pass things:
+    1. Serializables -> get passed by value, takes a copy of the buffer and send the copy to the client, they can't alter our copy
+    2. Pass things remotely (each of the remote clients is a subtype of remote, get passed by remote reference). All the contents are remote references.
+
+     */
+
+  }
+  def publicMessage(client: RemoteClient, text:String): Unit = { // Want to send things out to various clients
+    val message = client.name + " : " + text
+    clients.foreach(_.message(client,message))
+
+  }
+
+  private def sendUpdate: Unit = {
+    val deadClients = clients.filter { c =>
+      try {
+        c.clientUpdate(clients)
+        false
+      } catch {
+        case ex: RemoteException => true
+      }
+    }
+
+    clients --= deadClients
+
+  }
 }
